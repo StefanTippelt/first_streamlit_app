0966140378e7839df1ff795fb1a4ec64b2f83b58
Stefan Tippelt <stefantippelt@d72y6f7kdx.fritz.box>
2023-07-18 16:21:23 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show as dataframe
 streamlit.dataframe(fruityvice_normalized)
+
